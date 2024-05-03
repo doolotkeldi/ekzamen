b620decf45480937fef367f104e384d5a4c9553c
jm1a <borubaevaasiya@gmail.com>
2024-05-03 19:47:19 +0600

    Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,44 @@
 import React from 'react'
+import './Header.css'
+import logo from '../../assets/svg/logo.svg' 
+import search from '../../assets/svg/search.svg' 
+import six from '../../assets/svg/six.svg' 
+import Button from '../../assets/svg/Button.svg'
 
 function Header() {
   return (
-    <div>
-      header
-    </div>
+    <header>
+      <div className="container">
+        <div className="con-header">
+          <div className='logo-con'>
+            <img src={logo} alt="" />
+          </div>
+          <div className='text-con'>
+            <ul>
+              <li>Home</li>
+              <li>Shop</li>
+              <li>Plant Care</li>
+              <li>Blogs</li>
+            </ul>
+          </div>
+          <div className='button-con'>
+            <img src={search} alt="" />
+            <div className="six-con">
+            <img src={six} alt="" />
+            <div>6</div>
+            </div>
+            <div className="but-con">
+              <button>
+
+              <img src={Button} alt="" />
+                login
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
   )
 }
 
-export default Header
+export default Header 
