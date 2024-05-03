9a619938fa94160fc173fd4f1abd01cfc82a71b5
doolotkeldi <dolotalanov35@gmail.com>
2024-05-03 18:38:26 +0600

    first commit

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SectioonOne from '../SectionOne/SectioonOne'
+
+function Home() {
+  return (
+    <div>
+      <SectioonOne/>
+    </div>
+  )
+}
+
+export default Home
---
+++ src/components/SectionOne/SectioonOne.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SectioonOne() {
+  return (
+    <div>
+      sectionone
+    </div>
+  )
+}
+
+export default SectioonOne
